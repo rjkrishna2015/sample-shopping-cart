8d2fba91944ba5f832e366dd00530291381dcc51
rjkrishna2015 <rjkrishna2015@gmail.com>
2022-12-05 13:55:21 +0530

    updated backend url

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,7 +1,6 @@
 var API_PATH = "";
 if (process.env.NODE_ENV === "production") {
-  API_PATH =
-    "https://sample-shopping-cart-backend.herokuapp.com/getallproducts";
+  API_PATH = "https://sample-shopping-cart-backend.onrender.com/getallproducts";
 } else {
   API_PATH = "http://localhost:8759/getallproducts";
 }
